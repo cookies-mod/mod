ffd25edf3ea0c9d60995c27a61989c3a34f5606c
Moritz <59416038+Morazzer@users.noreply.github.com>
2024-08-01 17:45:24 +0200

    feat(PingDisplay): add a ping display (#67)

---
+++ src/main/java/dev/morazzer/cookies/mod/utils/mixins/PingMeasurerMixin.java
@@ -0,0 +1,21 @@
+package dev.morazzer.cookies.mod.utils.mixins;
+
+import dev.morazzer.cookies.mod.utils.CookiesUtils;
+import dev.morazzer.cookies.mod.utils.SkyblockUtils;
+import net.minecraft.client.network.PingMeasurer;
+import net.minecraft.network.packet.s2c.query.PingResultS2CPacket;
+import net.minecraft.util.Util;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PingMeasurer.class)
+public class PingMeasurerMixin {
+
+    @Inject(method = "onPingResult", at = @At(value = "HEAD"))
+    public void onPingResult(PingResultS2CPacket packet, CallbackInfo ci) {
+        SkyblockUtils.setLastPing(Util.getMeasuringTimeMs() - packet.startTime());
+    }
+
+}
