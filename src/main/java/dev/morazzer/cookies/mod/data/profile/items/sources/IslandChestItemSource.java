02c42193203b7e46270cd16ff862600644ee3a13
Moritz <59416038+Morazzer@users.noreply.github.com>
2024-10-07 11:42:46 +0200

    fix: prevent empty global profile data from crashing the game (#163)

--- src/main/java/dev/morazzer/cookies/mod/data/profile/items/sources/IslandChestItemSource.java
+++ src/main/java/dev/morazzer/cookies/mod/data/profile/items/sources/IslandChestItemSource.java
@@ -1,5 +1,10 @@
 package dev.morazzer.cookies.mod.data.profile.items.sources;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import dev.morazzer.cookies.mod.data.profile.ProfileData;
 import dev.morazzer.cookies.mod.data.profile.ProfileStorage;
 import dev.morazzer.cookies.mod.data.profile.items.Item;
@@ -7,15 +12,7 @@ import dev.morazzer.cookies.mod.data.profile.items.ItemSource;
 import dev.morazzer.cookies.mod.data.profile.items.ItemSources;
 import dev.morazzer.cookies.mod.data.profile.profile.GlobalProfileData;
 import dev.morazzer.cookies.mod.data.profile.profile.IslandChestStorage;
-
 import dev.morazzer.cookies.mod.utils.dev.FunctionUtils;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-
 import lombok.Getter;
 
 /**
@@ -31,13 +28,11 @@ public class IslandChestItemSource implements ItemSource<IslandChestStorage.Ches
 
 	@Override
 	public Collection<Item<?>> getAllItems() {
-		final Optional<ProfileData> optionalProfileData = ProfileStorage.getCurrentProfile();
-		if (optionalProfileData.isEmpty()) {
-			return Collections.emptySet();
-		}
-
-		final ProfileData profileData = optionalProfileData.get();
-		List<IslandChestStorage.ChestItem> items = profileData.getGlobalProfileData().getIslandStorage().getItems();
+		List<IslandChestStorage.ChestItem> items = ProfileStorage.getCurrentProfile()
+				.map(ProfileData::getGlobalProfileData)
+				.map(GlobalProfileData::getIslandStorage)
+				.map(IslandChestStorage::getItems)
+				.orElse(Collections.emptyList());
 
 		Collection<Item<?>> item = new ArrayList<>();
 		items.forEach(chestItem -> item.add(new Item<>(chestItem.itemStack(),
