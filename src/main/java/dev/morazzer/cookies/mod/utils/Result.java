8014844094534ba04dc0e61dbc866b5bc6667439
Moritz <59416038+Morazzer@users.noreply.github.com>
2024-10-30 00:03:16 +0100

    fix: add more verbose error messages to craft helper (#190)
    fix: add `Slimeball` and `Slime Block` to the default blocklist

--- src/main/java/dev/morazzer/cookies/mod/utils/Result.java
+++ src/main/java/dev/morazzer/cookies/mod/utils/Result.java
@@ -1,16 +1,19 @@
 package dev.morazzer.cookies.mod.utils;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface Result<T, E> {
 
-	static <T, E> Result<T, E> success(T value) {
+
+	static <T, E> Result<T, E> success(@NotNull T value) {
 		return new Success<>(value);
 	}
 
-	static <T, E> Result<T, E> error(E error) {
+	static <T, E> Result<T, E> error(@NotNull E error) {
 		return new Error<>(error);
 	}
 
@@ -26,10 +29,34 @@ public interface Result<T, E> {
 		return this instanceof Result.Error<T, E>;
 	}
 
-	record Success<T, E>(T type) implements Result<T, E> {
+	default Result<T, E> ifSuccess(Consumer<T> consumer) {
+		if (this instanceof Result.Success<T, E> success) {
+			consumer.accept(success.type());
+		}
+		return this;
+	}
+
+	default Result<T, E> ifError(Consumer<E> consumer) {
+		if (this instanceof Result.Error<T, E> error) {
+			consumer.accept(error.error());
+		}
+		return this;
+	}
+
+	@Nullable
+	default T unbox() {
+		return getResult().orElse(null);
+	}
+
+	@NotNull
+	default T unwrap() {
+		return getResult().orElseThrow(NullPointerException::new);
+	}
+
+	record Success<T, E>(@NotNull T type) implements Result<T, E> {
 		@Override
 		public Optional<T> getResult() {
-			return Optional.ofNullable(type);
+			return Optional.of(type);
 		}
 
 		@Override
@@ -49,5 +76,4 @@ public interface Result<T, E> {
 			return Optional.of(error);
 		}
 	}
-
 }
