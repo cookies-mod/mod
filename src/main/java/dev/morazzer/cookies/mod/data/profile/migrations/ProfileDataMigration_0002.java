0bdce830f7db2a788a2f4b091f10b0d4e20b86a7
Moritz <59416038+morazzer@users.noreply.github.com>
2024-10-30 11:44:58 +0100

    feat: backport failed migration recovery
    (cherry picked from commit ff6c8ff0b83b6c59c3df845c8216a8dc8a854eeb)

--- src/main/java/dev/morazzer/cookies/mod/data/profile/migrations/ProfileDataMigration_0002.java
+++ src/main/java/dev/morazzer/cookies/mod/data/profile/migrations/ProfileDataMigration_0002.java
@@ -13,14 +13,27 @@ public class ProfileDataMigration_0002 implements Migration<JsonObject> {
 
 	@Override
 	public void apply(JsonObject value) {
+
+		if (value == null || !value.has("selected_craft_helper_item")) {
+			return;
+		}
+		if (value.has("cause_fail_d77a3206-4aa8-4e17-a14f-8eba07fedec1")) {
+			throw new NullPointerException();
+		}
+
 		final JsonElement selectedCraftHelperItem = value.remove("selected_craft_helper_item");
 		if (selectedCraftHelperItem != null) {
 			JsonObject jsonObject = new JsonObject();
 			jsonObject.add("id", selectedCraftHelperItem);
-			jsonObject.addProperty("amount",2);
-			jsonObject.add("collapsed",new JsonArray());
+			jsonObject.addProperty("amount", 2);
+			jsonObject.add("collapsed", new JsonArray());
 			value.add("craft_helper_data", jsonObject);
 		}
+	}
+
+	@Override
+	public boolean mayFail() {
+		return true;
 	}
 
 	@Override
