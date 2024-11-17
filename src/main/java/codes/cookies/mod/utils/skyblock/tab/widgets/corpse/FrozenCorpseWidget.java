a2d17bdcf0772ac9192fec73d06cad73183dce5f
Morazzer <59416038+Morazzer@users.noreply.github.com>
2024-11-17 22:32:29 +0100

    update part of the widget parsing code

--- src/main/java/codes/cookies/mod/utils/skyblock/tab/widgets/corpse/FrozenCorpseWidget.java
+++ src/main/java/codes/cookies/mod/utils/skyblock/tab/widgets/corpse/FrozenCorpseWidget.java
@@ -1,11 +1,11 @@
 package codes.cookies.mod.utils.skyblock.tab.widgets.corpse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import codes.cookies.mod.utils.skyblock.tab.PlayerListReader;
 import codes.cookies.mod.utils.skyblock.tab.widgets.PlayerListWidget;
 import lombok.Getter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Widget with information about what corpses are present in the mineshaft.
@@ -18,12 +18,21 @@ public class FrozenCorpseWidget extends PlayerListWidget {
 		return "Frozen Corpses";
 	}
 
+	public static boolean doesMatch(String s) {
+		return s.startsWith(getTitle());
+	}
+
 	@Override
 	protected void read(PlayerListReader reader) {
+		int amount = 0;
 		while (reader.canRead() && !reader.peek().isBlank() && !reader.isTitle()) {
 			if (!addCorpse(reader.peek())) {
 				return;
 			}
+			if (amount > 5) {
+				return;
+			}
+			amount++;
 			reader.skip();
 		}
 	}
@@ -34,16 +43,18 @@ public class FrozenCorpseWidget extends PlayerListWidget {
 		}
 
 		final String[] parts = peek.split(":");
+		if (parts.length != 2) {
+			return false;
+		}
+		if (!"looted".equalsIgnoreCase(parts[1].trim()) && !"not looted".equalsIgnoreCase(parts[1].trim())) {
+			return false;
+		}
 
 		CorpseType corpseType = CorpseType.getCorpseTypeFromString(parts[0].trim());
-		boolean found = "looted".equals(parts[1]);
+		boolean found = "looted".equalsIgnoreCase(parts[1].trim());
 
 		corpses.add(new CorpseEntry(corpseType, found));
 
 		return true;
-	}
-
-	public static boolean doesMatch(String s) {
-		return s.startsWith(getTitle());
 	}
 }
