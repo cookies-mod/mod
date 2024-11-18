0b5663956b4c63bff3084c669c7ccbc2a6464ac9
GrahamKracker <86011515+grahamkracker@users.noreply.github.com>
2024-11-18 07:40:29 -0500

    add the signEditEnterSubmits behavior (#203, backport)
    * add the signEditEnterSubmits behavior
    
    * Update src/main/java/codes/cookies/mod/events/mixins/AbstractSignEditScreenMixin.java
    
    Co-authored-by: Moritz <59416038+Morazzer@users.noreply.github.com>
    
    * Update src/main/java/codes/cookies/mod/events/mixins/AbstractSignEditScreenMixin.java
    
    Co-authored-by: Moritz <59416038+Morazzer@users.noreply.github.com>
    
    * Update MiscConfig.java
    
    ---------
    
    Co-authored-by: Moritz <59416038+Morazzer@users.noreply.github.com>
    (cherry picked from commit d3e140f0a4ea6a43cab0294cc42b5784bbb8aa59)

--- src/main/java/codes/cookies/mod/translations/TranslationKeys.java
+++ src/main/java/codes/cookies/mod/translations/TranslationKeys.java
@@ -312,6 +312,7 @@ public interface TranslationKeys {
 	String CONFIG_MISC_EDIT_HUD = CONFIG_MISC + ".edit_hud";
 	String CONFIG_MISC_SHOW_MUSEUM_ARMOR_SETS = CONFIG_MISC + ".show_museum_armor_sets";
 	String CONFIG_MISC_ENABLE_SCROLL_TOOLTIPS = CONFIG_MISC + ".scroll_tooltips";
+	String CONFIG_MISC_SIGN_EDIT_ENTER_SUBMITS = CONFIG_MISC + ".sign_edit_enter_submits";
 	String CONFIG_MISC_STORAGE_PREVIEW = CONFIG_MISC + ".storage_preview";
 	String CONFIG_MISC_SHOW_PING = CONFIG_MISC + ".show_ping";
 	String CONFIG_MISC_CATEGORIES_ITEMS = CONFIG_MISC + CATEGORIES_PART + ".items";
