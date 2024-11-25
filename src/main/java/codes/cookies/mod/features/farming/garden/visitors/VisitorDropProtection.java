f3755efc012d24cb866f4f62a93fd81f95ecbfff
GrahamKracker <gjguzynski@gmail.com>
2024-11-25 09:52:57 -0500

    QOL changes, fix lore detection? only tested with copper

--- src/main/java/codes/cookies/mod/features/farming/garden/visitors/VisitorDropProtection.java
+++ src/main/java/codes/cookies/mod/features/farming/garden/visitors/VisitorDropProtection.java
@@ -7,15 +7,24 @@ import codes.cookies.mod.utils.cookies.CookiesUtils;
 import codes.cookies.mod.utils.exceptions.ExceptionHandler;
 import codes.cookies.mod.utils.items.CookiesDataComponentTypes;
 import codes.cookies.mod.utils.skyblock.LocationUtils;
+import codes.cookies.mod.utils.skyblock.inventories.ItemBuilder;
 import com.google.common.util.concurrent.Runnables;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.component.ComponentType;
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.CustomModelDataComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.text.Text;
 
+import net.minecraft.util.Rarity;
+
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class VisitorDropProtection {
 
@@ -30,21 +39,21 @@ public class VisitorDropProtection {
 		}
 	}
 
-
+	protected final ItemStack disabledItem = new ItemBuilder(Items.BARRIER).hideAdditionalTooltips().set(CookiesDataComponentTypes.ITEM_CLICK_RUNNABLE, Runnables.doNothing()).setName(Text.of("Refusing temporarily disabled due to rare drop")).set(DataComponentTypes.RARITY, Rarity.COMMON).build();
 
 	private void handleRejectButton(ItemStack rejectButton, boolean visitorItemLore)
 	{
-		if(rejectButton.isEmpty() || !rejectButton.isOf(Items.RED_CONCRETE_POWDER) || !visitorItemLore) {
+		if(rejectButton.isEmpty() || !rejectButton.isOf(Items.RED_TERRACOTTA) || !visitorItemLore) {
 			return;
 		}
 
-		CookiesUtils.sendMessage("Rejecting visitor drop protection");
 		rejectButton.set(CookiesDataComponentTypes.ITEM_CLICK_RUNNABLE, Runnables.doNothing());
+		rejectButton.set(CookiesDataComponentTypes.OVERRIDE_ITEM, disabledItem);
 		new java.util.Timer().schedule(
 				new java.util.TimerTask() {
 					@Override
 					public void run() {
-						CookiesUtils.sendMessage("Re-enabling visitor drop protection");
+						rejectButton.remove(CookiesDataComponentTypes.OVERRIDE_ITEM);
 						rejectButton.remove(CookiesDataComponentTypes.ITEM_CLICK_RUNNABLE);
 					}
 				},
@@ -58,29 +67,29 @@ public class VisitorDropProtection {
 		if(visitorItem == null || visitorItem.isEmpty() || !visitorItem.isOf(Items.GREEN_TERRACOTTA) || !visitorItem.contains(DataComponentTypes.LORE)) {
 			return false;
 		}
+		var lore = String.join("\n", visitorItem.get(DataComponentTypes.LORE).lines().stream().map(Text::getString).toArray(String[]::new));
+		Pattern pattern = Pattern.compile("Rewards:([\\S\\s]*)(?:Click|Missing)");
+		Matcher matcher = pattern.matcher(lore);
+		if (!matcher.find())
+		{
+			return false;
+		}
 
-        for (Text text : visitorItem.get(DataComponentTypes.LORE).lines()) {
-            if (text.getString().contains("+")) {
-                CookiesUtils.sendMessage("drop: " + text.getString());
+		for (String rareDrop : rareDrops) {
+			if (StringUtils.containsIgnoreCase(lore, rareDrop)) {
+				return true;
+			}
+		}
 
-				for (String rareDrop : rareDrops) {
-					if (text.getString().contains(rareDrop)) {
-						return true;
-					}
+		if(ConfigManager.getConfig().farmingConfig.visitorNotAsRareDropProtection.getValue()) {
+			for (String commonDrop : commonDrops) {
+				if (StringUtils.containsIgnoreCase(lore, commonDrop)) {
+					return true;
 				}
+			}
+		}
 
-				if(ConfigManager.getConfig().farmingConfig.visitorNotAsRareDropProtection.getValue()) {
-					for (String commonDrop : commonDrops) {
-						if (text.getString().contains(commonDrop)) {
-							return true;
-						}
-					}
-				}
-
-            }
-        }
-
-        return false;
+		return false;
 	}
 
 	private static final String[] rareDrops = new String[] {
