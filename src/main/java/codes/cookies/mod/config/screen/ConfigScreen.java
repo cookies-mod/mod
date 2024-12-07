039d1b039076c7a911ed8b7d58c7c16e73e67bc5
Morazzer <59416038+Morazzer@users.noreply.github.com>
2024-12-07 10:53:55 +0100

    fix: config closing when pressing inventory key
    (cherry picked from commit 62f93a4e7990573b4f231ba14658e6147a1a58d4)

--- src/main/java/codes/cookies/mod/config/screen/ConfigScreen.java
+++ src/main/java/codes/cookies/mod/config/screen/ConfigScreen.java
@@ -159,7 +159,7 @@ public class ConfigScreen extends ScrollbarScreen implements InventoryConfigScre
 		if (hasBeenConsumed.get()) {
 			return true;
 		}
-		if (MinecraftClient.getInstance().options.inventoryKey.matchesKey(keyCode, scanCode)) {
+		if (!searchField.isFocused() && MinecraftClient.getInstance().options.inventoryKey.matchesKey(keyCode, scanCode)) {
 			this.close();
 			return true;
 		}
