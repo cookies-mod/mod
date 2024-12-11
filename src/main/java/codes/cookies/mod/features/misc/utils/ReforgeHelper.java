cd698bad772a231564ce942ec41168acc5723a72
GrahamKracker <gjguzynski@gmail.com>
2024-12-11 14:20:23 -0500

    initial commit

--- src/main/java/codes/cookies/mod/features/misc/utils/ReforgeHelper.java
+++ src/main/java/codes/cookies/mod/features/misc/utils/ReforgeHelper.java
@@ -2,14 +2,28 @@ package codes.cookies.mod.features.misc.utils;
 
 import codes.cookies.mod.config.ConfigManager;
 import codes.cookies.mod.events.InventoryEvents;
+import codes.cookies.mod.utils.cookies.CookiesUtils;
 import codes.cookies.mod.utils.skyblock.LocationUtils;
 
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerListener;
 
 public class ReforgeHelper {
 	public ReforgeHelper(HandledScreen<?> handledScreen) {
-		
+		handledScreen.getScreenHandler().addListener(new ScreenHandlerListener() {
+			@Override
+			public void onSlotUpdate(ScreenHandler handler, int slotId, ItemStack stack) {
+				CookiesUtils.sendMessage("onSlotUpdate: " + slotId + " " + stack);
+			}
+
+			@Override
+			public void onPropertyUpdate(ScreenHandler handler, int property, int value) {
+				CookiesUtils.sendMessage("onPropertyUpdate: " + property + " " + value);
+			}
+		});
 	}
 
 	public static void init() {
