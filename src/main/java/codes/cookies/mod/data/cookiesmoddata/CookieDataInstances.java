aa945ec453fb85c97db5148e3dcf610d910a62a1
Morazzer <59416038+Morazzer@users.noreply.github.com>
2024-12-13 21:58:48 +0100

    fix: garden keybinds sometimes resetting on restart
    (cherry picked from commit e25a31db39ba6ac6526ec61c1173ea0d4c69a0b8)

--- src/main/java/codes/cookies/mod/data/cookiesmoddata/CookieDataInstances.java
+++ src/main/java/codes/cookies/mod/data/cookiesmoddata/CookieDataInstances.java
@@ -2,6 +2,22 @@ package codes.cookies.mod.data.cookiesmoddata;
 
 import codes.cookies.mod.data.farming.GardenKeybindsData;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+
 public final class CookieDataInstances {
-	public static final GardenKeybindsData gardenKeybindsData = new GardenKeybindsData();
+	public static final GardenKeybindsData gardenKeybindsData = register(new GardenKeybindsData());
+
+	/**
+	 * Registers the data and in all required positions.
+	 * @param data The data to register.
+	 * @return The data.
+	 * @param <T> The type of the data.
+	 */
+	private static <T extends CookiesModData> T register(T data) {
+		if (data instanceof ClientInitializationCallback callback) {
+			ClientLifecycleEvents.CLIENT_STARTED.register(callback::gameInitialized);
+		}
+
+		return data;
+	}
 }
